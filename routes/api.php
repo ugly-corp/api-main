1291ef4aa4d80e0aaeae057780996966bc7827cb
NookieNoo <biv0208@yandex.ru>
2024-06-22 23:58:02 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,24 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::prefix('/post')->group(function (): void {
-    Route::get('/{id}', [PostController::class, 'show'])->where('id', '[0-9]+');
-    Route::get('/', [PostController::class, 'index']);
+Route::prefix('/v1')->group(function (): void {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+
+    Route::prefix('/post')->group(function (): void {
+        Route::get('/{id}', [PostController::class, 'show'])->where('id', '[0-9]+');
+        Route::get('/', [PostController::class, 'index']);
+    });
+
+    Route::prefix('/category')->group(function (): void {
+        Route::get('/{id}', [CategoryController::class, 'show'])->where('id', '[0-9]+');
+        Route::get('/', [CategoryController::class, 'index']);
+    });
 });
